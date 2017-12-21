bd5266624b0e6b33a52772d2d051c92cabdff232
Leo Balduf <leobalduf@gmail.com>
2017-12-21 23:32:53 +0100

    make state easier to work with

--- state.go
+++ state.go
@@ -8,42 +8,43 @@ import (
 )
 
 // LiveState is a helper struct to keep track of Live API updates.
+// Access must be protected with the embedded mutex.
 type LiveState struct {
-	machines           []MachineEntry
-	devicesLastUpdated map[string]time.Time
-	l                  sync.Mutex
+	Machines           []MachineEntry
+	DevicesLastUpdated map[string]time.Time
+	sync.Mutex
 }
 
 // NewLiveState returns a new LiveState.
 func NewLiveState() *LiveState {
 	return &LiveState{
-		devicesLastUpdated: make(map[string]time.Time),
+		DevicesLastUpdated: make(map[string]time.Time),
 	}
 }
 
 // SetSystemInfo clears the current state and sets it to the state received.
 // The machine entries are kept as a reference, do not modify them later on.
 func (s *LiveState) SetSystemInfo(entries []MachineEntry) {
-	s.l.Lock()
-	defer s.l.Unlock()
+	s.Lock()
+	defer s.Unlock()
 
-	s.machines = entries
-	s.devicesLastUpdated = make(map[string]time.Time)
+	s.Machines = entries
+	s.DevicesLastUpdated = make(map[string]time.Time)
 }
 
 // AddMachine adds a machine entry if it's not present already.
 // If it is, the entry is overwritten.
 func (s *LiveState) AddMachine(entry MachineEntry) {
-	s.l.Lock()
-	defer s.l.Unlock()
-	for i, m := range s.machines {
+	s.Lock()
+	defer s.Unlock()
+	for i, m := range s.Machines {
 		if m.SID == entry.SID {
-			s.machines[i] = entry
+			s.Machines[i] = entry
 			return
 		}
 	}
 
-	s.machines = append(s.machines, entry)
+	s.Machines = append(s.Machines, entry)
 }
 
 // Update updates the state with the given status change.
@@ -51,17 +52,17 @@ func (s *LiveState) AddMachine(entry MachineEntry) {
 // If that happens, the state got out of sync somehow.
 // Best close and re-open the websocket connection and rebuild the state.
 func (s *LiveState) Update(container StatusChangeContainer) error {
-	s.l.Lock()
-	defer s.l.Unlock()
-	for i, m := range s.machines {
+	s.Lock()
+	defer s.Unlock()
+	for i, m := range s.Machines {
 		if m.SID == container.MachineSID {
 			for j, d := range m.Devices {
 				if d.ID == container.DeviceID {
 					d.Status = container.Status
 
 					m.Devices[j] = d
-					s.machines[i] = m
-					s.devicesLastUpdated[container.DeviceID] = time.Now()
+					s.Machines[i] = m
+					s.DevicesLastUpdated[container.DeviceID] = time.Now()
 
 					return nil
 				}
